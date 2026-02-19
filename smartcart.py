d33409e32ccf45f87945b5be299ba05c63915315
NitishVashishtha03 <nitishvashissth@gmail.com>
2026-02-19 14:44:15 +0530

    Add files via upload

--- smartcart.py
+++ smartcart.py
@@ -1,99 +1,273 @@
-# ================================
-# SMARTCART CUSTOMER CLUSTERING APP
-# ================================
+# Generated from: smartcart.ipynb
+# Converted at: 2026-02-19T05:33:31.836Z
+# Next step (optional): refactor into modules & generate tests with RunCell
+# Quick start: pip install runcell
 
-import streamlit as st
 import pandas as pd
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-st.set_page_config(page_title="SmartCart Clustering", layout="centered")
+df = pd.read_csv("smartcart_customers.csv")
 
-st.title("🛒 SmartCart Customer Clustering System")
+df.head()
 
-# ================================
-# LOAD DATA
-# ================================
-@st.cache_data
-def load_data():
-    df = pd.read_csv("smartcart_customers.csv")
-    return df
+df.shape
 
-df = load_data()
+df.isnull().sum()
 
-st.write("Dataset Shape:", df.shape)
+# # Data Preprocessing
 
-# ================================
-# SELECT FEATURES
-# ================================
-features = [
-    'Income','MntWines','MntFruits','MntMeatProducts',
-    'MntFishProducts','MntSweetProducts','MntGoldProds',
-    'NumWebPurchases','NumStorePurchases','Recency'
-]
 
-X = df[features]
+# ## 1. Handle Missing Values
 
-# ================================
-# FIX MISSING VALUES  🔥 IMPORTANT
-# ================================
-X = X.fillna(X.median())
 
-# ================================
-# SCALE DATA
-# ================================
+df["Income"] = df["Income"].fillna(df["Income"].median())
+
+df.head()
+
+# ## Feature engineering
+
+
+# Age
+df["Age"] = 2026-df["Year_Birth"]
+
+# Customer Joining Date
+df["Dt_Customer"] = pd.to_datetime(df["Dt_Customer"], dayfirst=True)
+
+reference_date = df["Dt_Customer"].max()
+
+df["Customer_Tenure_Days"] = (reference_date - df["Dt_Customer"]).dt.days
+
+# Spending
+
+df["Total_Spending"] = df["MntWines"] + df["MntFruits"] + df["MntMeatProducts"]  + df["MntFishProducts"] + df["MntSweetProducts"] + df["MntGoldProds"]
+
+# Children
+df["Total_Children"] = df["Kidhome"] + df["Teenhome"]
+
+# Education
+
+df["Education"].value_counts()
+
+df["Education"] = df["Education"].replace({
+    "Basic": "Undergraduate", "2n Cycle": "Undergraduate",
+    "Graduation": "Graduate",
+    "Master": "Postgraduate", "PhD": "Postgraduate"
+})
+
+# Marital Status
+
+df["Living_With"] = df["Marital_Status"].replace({
+    "Married": "Partner", "Together": "Partner",
+    "Single": "Alone", "Divorced": "Alone",
+    "Widow": "Alone", "Absurd": "Alone", "YOLO": "Alone"
+})
+
+# ## Drop Columns
+
+
+df.head()
+
+cols = ["ID", "Year_Birth", "Marital_Status", "Kidhome", "Teenhome", "Dt_Customer"]
+spending_cols = ["MntWines", "MntFruits", "MntMeatProducts", "MntFishProducts", "MntSweetProducts", "MntGoldProds"]
+
+cols_to_drop = cols + spending_cols
+
+df_cleaned = df.drop(columns=cols_to_drop)
+
+df_cleaned.shape
+
+df_cleaned.head()
+
+# # Outliers
+
+
+cols = ["Income", "Recency", "Response", "Age", "Total_Spending", "Total_Children"]
+
+# relative plots of some features-  pair plots
+sns.pairplot(df_cleaned[cols])
+
+# Remove outliers
+
+print("data size with outliers:", len(df_cleaned))
+
+df_cleaned = df_cleaned[ (df_cleaned["Age"] < 90) ]
+df_cleaned = df_cleaned[ (df_cleaned["Income"] < 600_000) ]
+
+print("data size without outliers:", len(df_cleaned))
+
+# # Heatmap
+
+
+corr = df_cleaned.corr(numeric_only=True)
+
+plt.figure(figsize=(8, 6))
+
+sns.heatmap(
+    corr,
+    annot=True,
+    annot_kws={"size": 6},
+    cmap="coolwarm"
+)
+
+df_cleaned.shape
+
+df_cleaned.head()
+
+# # Encoding
+
+
+from sklearn.preprocessing import OneHotEncoder
+
+ohe = OneHotEncoder()
+
+cat_cols = ["Education", "Living_With"]
+
+enc_cols = ohe.fit_transform(df_cleaned[cat_cols])
+
+enc_df = pd.DataFrame(enc_cols.toarray(), columns=ohe.get_feature_names_out(cat_cols), index=df_cleaned.index)
+
+df_encoded = pd.concat([df_cleaned.drop(columns=cat_cols),enc_df], axis=1)
+
+df_encoded.shape
+
+df_encoded.head()
+
+# # Scaling
+
+
+from sklearn.preprocessing import StandardScaler
+
+X = df_encoded
+
 scaler = StandardScaler()
+
 X_scaled = scaler.fit_transform(X)
 
-# ================================
-# TRAIN MODEL
-# ================================
-kmeans = KMeans(n_clusters=4, random_state=42, n_init=10)
-kmeans.fit(X_scaled)
+# # Visualize
 
-# ================================
-# SHOW CLUSTER DISTRIBUTION
-# ================================
-st.subheader("Cluster Distribution")
 
-df["Cluster"] = kmeans.labels_
+X_scaled.shape
 
-fig, ax = plt.subplots()
-sns.countplot(x=df["Cluster"], ax=ax)
-st.pyplot(fig)
+# 2D 
+from sklearn.decomposition import PCA
 
-# ================================
-# USER INPUT
-# ================================
-st.subheader("👉 Enter Customer Details")
+pca = PCA(n_components=3)
 
-income = st.number_input("Income", 0)
-wine = st.number_input("Wine Spending", 0)
-fruit = st.number_input("Fruit Spending", 0)
-meat = st.number_input("Meat Spending", 0)
-fish = st.number_input("Fish Spending", 0)
-sweet = st.number_input("Sweet Spending", 0)
-gold = st.number_input("Gold Spending", 0)
-web = st.number_input("Web Purchases", 0)
-store = st.number_input("Store Purchases", 0)
-recency = st.number_input("Days Since Last Purchase", 0)
+X_pca = pca.fit_transform(X_scaled)
 
-# ================================
-# PREDICT
-# ================================
-if st.button("Predict Customer Cluster"):
+pca.explained_variance_ratio_
 
-    user_data = np.array([[income,wine,fruit,meat,fish,sweet,gold,web,store,recency]])
-    user_scaled = scaler.transform(user_data)
-    cluster = kmeans.predict(user_scaled)
+# plot
+fig = plt.figure(figsize=(8, 6))
 
-    st.success(f"✅ Customer belongs to Cluster: {cluster[0]}")
+ax = fig.add_subplot(111, projection="3d")
 
-# ================================
-# SHOW DATA
-# ================================
-if st.checkbox("Show Dataset"):
-    st.dataframe(df.head())
+ax.scatter(X_pca[:, 0], X_pca[:, 1], X_pca[:, 2])
+
+ax.set_xlabel("PCA1")
+ax.set_ylabel("PCA2")
+ax.set_zlabel("PCA3")
+ax.set_title("3d projection")
+
+# # Analyze K value
+# ## 1. Elbow Method
+
+
+from sklearn.cluster import KMeans
+from kneed import KneeLocator
+
+wcss = []
+for k in range(1, 11):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit_predict(X_pca)
+    wcss.append(kmeans.inertia_)
+    
+
+knee = KneeLocator(range(1, 11), wcss, curve="convex", direction="decreasing")
+optimal_k = knee.elbow
+
+print("best k =", optimal_k)
+
+# plot
+
+plt.plot(range(1, 11), wcss, marker='o')
+plt.xlabel("K")
+plt.ylabel("WCSS")
+
+# ## 2. Silhouette Score
+
+
+from sklearn.metrics import silhouette_score
+
+scores = []
+
+for k in range(2, 11):
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    labels = kmeans.fit_predict(X_pca)
+    score = silhouette_score(X_pca, labels)
+    scores.append(score)
+
+# plot
+plt.plot(range(2, 11), scores, marker='o')
+plt.xlabel("K")
+plt.ylabel("Silhouette score")
+
+# combined plot
+
+k_range = range(2, 11)
+
+fig, ax1 = plt.subplots(figsize=(8, 6))
+
+ax1.plot(k_range, wcss[:len(k_range)], marker="o", color="blue") 
+ax1.set_xlabel("K")
+ax1.set_ylabel("WCSS")
+
+ax2 = ax1.twinx()
+ax2.plot(k_range, scores[:len(k_range)], marker="x", color="red", linestyle="--")
+ax2.set_ylabel("SS")
+
+# # Clustering
+
+
+# K_means
+
+kmeans = KMeans(n_clusters=4, random_state=42)
+labels_kmeans = kmeans.fit_predict(X_pca)
+
+fig = plt.figure(figsize=(8, 6))
+
+ax = fig.add_subplot(111, projection="3d")
+
+ax.scatter(X_pca[:, 0], X_pca[:, 1], X_pca[:, 2], c=labels_kmeans)
+
+# Agglomerative Clustering
+from sklearn.cluster import AgglomerativeClustering
+
+agg_clf = AgglomerativeClustering(n_clusters=4, linkage="ward")
+labels_agg = agg_clf.fit_predict(X_pca)
+
+fig = plt.figure(figsize=(8, 6))
+ax = fig.add_subplot(111, projection="3d")
+ax.scatter(X_pca[:, 0], X_pca[:, 1], X_pca[:, 2], c=labels_agg)
+
+# # Characterization of Clusters
+
+
+
+X["cluster"] = labels_agg
+
+X.head()
+
+pal = ["red", "blue", "yellow", "green"]
+
+sns.countplot(x=X["cluster"], palette=pal, hue=X["cluster"])
+
+# Income & Spending patterns
+
+sns.scatterplot(x=X["Total_Spending"], y=X["Income"], hue=X["cluster"], palette=pal)
+
+# Cluster Summary
+
+cluster_summary = X.groupby("cluster").mean()
+print(cluster_summary)
